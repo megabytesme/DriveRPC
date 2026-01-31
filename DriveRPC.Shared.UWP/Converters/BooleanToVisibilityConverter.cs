72dd3d4800ecf0cf8a9221d194d89dc1ed7afd64
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2026-01-31 03:50:00 +0000

    Added boolean to visibility converter

---
+++ DriveRPC.Shared.UWP/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace DriveRPC.Shared.UWP.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public bool Invert { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool flag = value is bool b && b;
+
+            if (Invert)
+                flag = !flag;
+
+            return flag ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
