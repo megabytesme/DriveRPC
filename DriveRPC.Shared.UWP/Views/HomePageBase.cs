496f3e08eef994f0cff7580f4cc84514e30d83e2
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2026-01-31 22:58:44 +0000

    Migrated HomePage to use pagebase (reduce code duplication)

---
+++ DriveRPC.Shared.UWP/Views/HomePageBase.cs
@@ -0,0 +1,104 @@
+﻿using DriveRPC.Shared.ViewModels;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DriveRPC.Shared.UWP.Views
+{
+    public abstract class HomePageBase : Page
+    {
+        protected StatusViewModel ViewModel { get; private set; }
+        protected StatusCardViewModel CardViewModel { get; private set; }
+
+        private TextBlock _statusTextBlock;
+        private Controls.StatusCard _statusCardControl;
+
+        protected void InitializeSharedLogic(
+            TextBlock statusTextBlock,
+            Controls.StatusCard statusCardControl,
+            StatusViewModel viewModel)
+        {
+            _statusTextBlock = statusTextBlock;
+            _statusCardControl = statusCardControl;
+
+            ViewModel = viewModel;
+            DataContext = ViewModel;
+
+            CardViewModel = new StatusCardViewModel();
+            _statusCardControl.DataContext = CardViewModel;
+
+            SyncAllProperties();
+
+            ViewModel.PropertyChanged += OnViewModelPropertyChanged;
+
+            UpdateStatusText();
+        }
+
+        private void SyncAllProperties()
+        {
+            CardViewModel.ActivityName = ViewModel.ActivityName;
+            CardViewModel.ActivityDetails = ViewModel.ActivityDetails;
+            CardViewModel.ActivityState = ViewModel.ActivityState;
+            CardViewModel.ElapsedTimeText = ViewModel.ElapsedTimeText;
+            CardViewModel.PartyText = ViewModel.PartyText;
+            CardViewModel.LargeImageUrl = ViewModel.LargeImageUrl;
+            CardViewModel.SmallImageUrl = ViewModel.SmallImageUrl;
+        }
+
+        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(ViewModel.ActivityName):
+                    CardViewModel.ActivityName = ViewModel.ActivityName;
+                    break;
+
+                case nameof(ViewModel.ActivityDetails):
+                    CardViewModel.ActivityDetails = ViewModel.ActivityDetails;
+                    break;
+
+                case nameof(ViewModel.ActivityState):
+                    CardViewModel.ActivityState = ViewModel.ActivityState;
+                    break;
+
+                case nameof(ViewModel.ElapsedTimeText):
+                    CardViewModel.ElapsedTimeText = ViewModel.ElapsedTimeText;
+                    break;
+
+                case nameof(ViewModel.PartyText):
+                    CardViewModel.PartyText = ViewModel.PartyText;
+                    break;
+
+                case nameof(ViewModel.LargeImageUrl):
+                    CardViewModel.LargeImageUrl = ViewModel.LargeImageUrl;
+                    break;
+
+                case nameof(ViewModel.SmallImageUrl):
+                    CardViewModel.SmallImageUrl = ViewModel.SmallImageUrl;
+                    break;
+
+                case nameof(ViewModel.StatusText):
+                    UpdateStatusText();
+                    break;
+            }
+        }
+
+        protected async void StartRpc_Click(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.StartAsync();
+            UpdateStatusText();
+        }
+
+        protected async void StopRpc_Click(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.StopAsync();
+            UpdateStatusText();
+        }
+
+        protected void UpdateStatusText()
+        {
+            if (_statusTextBlock != null)
+                _statusTextBlock.Text = $"Status: {ViewModel.StatusText}";
+        }
+    }
+}
--- UWP_1507/Views/Win10_1507/HomePage_Win10_1507.xaml.cs
+++ UWP_1507/Views/Win10_1507/HomePage_Win10_1507.xaml.cs
@@ -1,34 +1,16 @@
 ﻿using DriveRPC.Shared.UWP.Services;
 using DriveRPC.Shared.ViewModels;
-using System;
-using System.ComponentModel;
 using UWP_1507;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media.Imaging;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace DriveRPC.Shared.UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class HomePage_Win10_1507 : Page
+    public sealed partial class HomePage_Win10_1507 : HomePageBase
     {
-        public StatusViewModel ViewModel { get; }
-
-        private string _lastLargeUrl;
-        private BitmapImage _largeBitmap;
-
-        private string _lastSmallUrl;
-        private BitmapImage _smallBitmap;
-
         public HomePage_Win10_1507()
         {
             InitializeComponent();
 
-            ViewModel = new StatusViewModel(
+            var viewModel = new StatusViewModel(
                 App.RpcController,
                 new UiThread(),
                 App.PresetService,
@@ -37,72 +19,7 @@ namespace DriveRPC.Shared.UWP.Views
                 App.ReverseGeocoder
             );
 
-            DataContext = ViewModel;
-
-            StatusCardControl.DataContext = new StatusCardViewModel();
-
-            var card = StatusCardControl.ViewModel;
-
-            card.ActivityName = ViewModel.ActivityName;
-            card.ActivityDetails = ViewModel.ActivityDetails;
-            card.ActivityState = ViewModel.ActivityState;
-            card.ElapsedTimeText = ViewModel.ElapsedTimeText;
-            card.PartyText = ViewModel.PartyText;
-
-            card.LargeImageUrl = ViewModel.LargeImageUrl;
-            card.SmallImageUrl = ViewModel.SmallImageUrl;
-
-            UpdateStatusText();
-            ViewModel.PropertyChanged += OnViewModelPropertyChanged;
-        }
-
-        private async void StartRpc_Click(object sender, RoutedEventArgs e)
-        {
-            await ViewModel.StartAsync();
-            UpdateStatusText();
-        }
-
-        private async void StopRpc_Click(object sender, RoutedEventArgs e)
-        {
-            await ViewModel.StopAsync();
-            UpdateStatusText();
-        }
-
-        private void UpdateStatusText()
-        {
-            StatusTextBlock.Text = $"Status: {ViewModel.StatusText}";
-        }
-
-        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            var card = StatusCardControl.ViewModel;
-
-            if (e.PropertyName == nameof(ViewModel.ActivityName))
-                card.ActivityName = ViewModel.ActivityName;
-
-            if (e.PropertyName == nameof(ViewModel.ActivityDetails))
-                card.ActivityDetails = ViewModel.ActivityDetails;
-
-            if (e.PropertyName == nameof(ViewModel.ActivityState))
-                card.ActivityState = ViewModel.ActivityState;
-
-            if (e.PropertyName == nameof(ViewModel.ElapsedTimeText))
-                card.ElapsedTimeText = ViewModel.ElapsedTimeText;
-
-            if (e.PropertyName == nameof(ViewModel.PartyText))
-                card.PartyText = ViewModel.PartyText;
-
-            if (e.PropertyName == nameof(ViewModel.LargeImageUrl))
-            {
-                if (card.LargeImageUrl != ViewModel.LargeImageUrl)
-                    card.LargeImageUrl = ViewModel.LargeImageUrl;
-            }
-
-            if (e.PropertyName == nameof(ViewModel.SmallImageUrl))
-            {
-                if (card.SmallImageUrl != ViewModel.SmallImageUrl)
-                    card.SmallImageUrl = ViewModel.SmallImageUrl;
-            }
+            InitializeSharedLogic(StatusTextBlock, StatusCardControl, viewModel);
         }
     }
 }
