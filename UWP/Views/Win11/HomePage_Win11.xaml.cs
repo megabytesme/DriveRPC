01c3d545c177ca5bc9aaf0bbf2be9b1311568159
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2026-02-01 11:08:06 +0000

    Added (initial) HomePage implementation for Windows 11 theme (UWP)

--- UWP/Views/Win11/HomePage_Win11.xaml.cs
+++ UWP/Views/Win11/HomePage_Win11.xaml.cs
@@ -1,30 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using DriveRPC.Shared.UWP.Services;
+using DriveRPC.Shared.ViewModels;
+using UWP;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace UWP.Views
+namespace DriveRPC.Shared.UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class HomePage_Win11 : Page
+    public sealed partial class HomePage_Win11 : HomePageBase
     {
         public HomePage_Win11()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            var viewModel = new StatusViewModel(
+                App.RpcController,
+                new UiThread(),
+                App.PresetService,
+                null,
+                App.GpsService,
+                App.ReverseGeocoder
+            );
+
+            InitializeSharedLogic(StatusTextBlock, StatusCardControl, viewModel);
         }
     }
 }
