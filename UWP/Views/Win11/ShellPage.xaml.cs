e8190c20e1b25e8af60a7bd05154614333c86d8a
MegaBytesMe <57240557+megabytesme@users.noreply.github.com>
2026-02-01 11:07:30 +0000

    Updated ShellPage (UWP - Windows 11 theme) to have account flyout

--- UWP/Views/Win11/ShellPage.xaml.cs
+++ UWP/Views/Win11/ShellPage.xaml.cs
@@ -1,11 +1,11 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Controls;
 using DriveRPC.Shared.UWP.Helpers;
-using DriveRPC.Shared.UWP.Services;
-using System;
-using NavigationViewSelectionChangedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs;
-using NavigationView = Microsoft.UI.Xaml.Controls.NavigationView;
-using NavigationViewItem = Microsoft.UI.Xaml.Controls.NavigationViewItem;
+using DriveRPC.Shared.UWP.Controls;
 
 namespace UWP.Views
 {
@@ -15,32 +15,59 @@ namespace UWP.Views
         {
             this.InitializeComponent();
 
-            if (NavView != null && NavView.MenuItems.Count > 0)
+            if (NavView.MenuItems.Count > 0)
             {
                 NavView.SelectedItem = NavView.MenuItems[0];
                 NavigateTo("Home");
             }
+
+            var authTask = HeaderAccountControl.SetupAsync(App.SecureStorage);
         }
 
-        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        private void NavView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
         {
             if (args.IsSettingsSelected)
             {
                 NavigateTo("Settings");
             }
-            else if (args.SelectedItem is NavigationViewItem item)
+            else if (args.SelectedItem is Microsoft.UI.Xaml.Controls.NavigationViewItem item)
             {
-                NavigateTo(item.Tag.ToString());
+                string tag = item.Tag?.ToString();
+                if (tag == "Account")
+                {
+                    FlyoutBase.ShowAttachedFlyout(item);
+                    return;
+                }
+                NavigateTo(tag);
             }
         }
 
         private void NavigateTo(string tag)
         {
             Type pageType = NavigationHelper.GetPageType(tag);
-
-            if (ContentFrame.CurrentSourcePageType != pageType)
+            if (pageType != null && ContentFrame.CurrentSourcePageType != pageType)
             {
                 ContentFrame.Navigate(pageType);
+            }
+        }
+
+        private void HeaderAccountControl_LoadingStateChanged(object sender, bool isLoading)
+        {
+            AccountLoadingRing.IsActive = isLoading;
+            AccountPersonPicture.Opacity = isLoading ? 0.3 : 1.0;
+        }
+
+        private void HeaderAccountControl_UserChanged(object sender, DiscordAccountControl.UserChangedEventArgs e)
+        {
+            if (e.User != null)
+            {
+                AccountPersonPicture.ProfilePicture = new BitmapImage(new Uri(e.User.GetAvatarUrl()));
+                AccountNameText.Text = e.User.GetDisplayName();
+            }
+            else
+            {
+                AccountPersonPicture.ProfilePicture = null;
+                AccountNameText.Text = "Account";
             }
         }
     }
